1c0a9644926a8d6321e92d178ece155545ea4c9d
Hugh O'Reilly <horeilly1101@gmail.com>
2018-08-13 18:00:29 -0500

    login route

--- runtrack_app/routes.py
+++ runtrack_app/routes.py
@@ -1,11 +1,22 @@
 from runtrack_app import app
-from flask import render_template
+from flask import render_template, url_for, request
+from .forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
 def index():
 	return render_template("base.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-	pass
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(email=form.email.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid email or password')
+			return redirect(url_for('login'))
+		login_user(user, remember=form.remember_me.data)
+		return redirect(url_for('index'))
+	return render_template('login.html', title='Login', form=form)
