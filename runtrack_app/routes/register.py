4b2460959c59691288f529778a1d5a105cb98425
Hugh O'Reilly <horeilly1101@gmail.com>
2018-08-13 18:48:54 -0500

    register route

--- runtrack_app/routes/register.py
+++ runtrack_app/routes/register.py
@@ -3,4 +3,18 @@ from flask import render_template, url_for, request
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-	pass
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(email=form.email.data, name=form.name.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+
+		login_user(user)
+		flash("Welcome to runtrack!")
+		return redirect(url_for('index'))
+
+	return render_template("register.html", form=form)
