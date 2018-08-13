95fc66e01dc01633530792cc3603c970e632eaa2
Hugh O'Reilly <horeilly1101@gmail.com>
2018-08-13 18:06:25 -0500

    base template and bootstrap

--- runtrack_app/forms.py
+++ runtrack_app/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from .models import User
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
-    submit = SubmitField('Sign In')
 
 class RegistrationForm(FlaskForm):
     name = StringField('Username', validators=[DataRequired()])
