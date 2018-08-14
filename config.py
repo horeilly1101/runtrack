eb204c7dedb8641fbf66e1ccd1595f22f99ae440
Hugh O'Reilly <horeilly1101@gmail.com>
2018-08-14 11:41:27 -0500

    comment

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
-	# Allow us to work locally with a PostgreSQl db
+	# Allows me to work locally with a PostgreSQl db
 	SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost:5555/postgres"
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
