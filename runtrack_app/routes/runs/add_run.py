d9901b4bd7110dbf906133c14dda01ac70846396
Hugh O'Reilly <horeilly1101@gmail.com>
2018-08-14 20:27:24 -0500

    database query

--- runtrack_app/routes/runs/add_run.py
+++ runtrack_app/routes/runs/add_run.py
@@ -1,8 +1,9 @@
-from runtrack_app import app
-from flask import render_template, url_for, request
+from runtrack_app import app, db
+from flask import render_template, url_for, request, redirect
 from flask_login import current_user, login_required
 from runtrack_app.forms import AddRunForm
 from runtrack_app.models import Run
+from datetime import datetime
 
 @app.route("/add_run", methods=["GET", "POST"])
 @login_required
@@ -12,7 +13,11 @@ def add_run():
 
 	if form.validate_on_submit():
 		run = Run(distance=form.distance.data, user_id = user.id)
+		start_datetime = datetime.combine(form.start_date.data, form.start_time.data)
+		run.started_at = start_datetime
 		db.session.add(run)
 		db.session.commit()
+
+		return redirect('runs')
 	return render_template("runs/add_run.html",
 		form=form)
