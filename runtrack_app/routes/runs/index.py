52b9e791b3075a1d5f658a1b7ca9829dd518190f
Hugh O'Reilly <horeilly1101@gmail.com>
2018-08-15 22:05:09 -0500

    Chart.js integration

--- runtrack_app/routes/runs/index.py
+++ runtrack_app/routes/runs/index.py
@@ -6,4 +6,13 @@ from flask_login import current_user, login_required
 @app.route("/index")
 @login_required
 def index():
-	return render_template("runs/index.html")
+	legend = 'Monthly Data'
+	labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+	values = [10, 9, 8, 7, 6, 4, 7, 20]
+
+	days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+	runs = [1, 2, 3, 4, 5, 6, 7]
+
+	return render_template("runs/index.html", values=values, labels=labels, legend=legend,
+		days=days,
+		runs=runs)
