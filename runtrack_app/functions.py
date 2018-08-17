624beab0f7a80d7f34231e65df81549b2e1a765e
Hugh O'Reilly <horeilly1101@gmail.com>
2018-08-16 21:29:56 -0500

    completed charts

--- runtrack_app/functions.py
+++ runtrack_app/functions.py
@@ -17,7 +17,7 @@ def sort_runs(runs):
 	sorted_runs.sort(key = lambda run: run.started_at)
 	return sorted_runs
 
-def group_runs(runs, start_date=None, end_date=date.today(), distances=False):
+def group_runs_daily(runs, start_date=None, end_date=date.today(), distances=False):
 	'''
 	returns a list of lists that groups runs according to their days,
 	between start_date and end_date (inclusive)
@@ -52,6 +52,7 @@ def group_runs(runs, start_date=None, end_date=date.today(), distances=False):
 				daily_runs.append(sorted_runs.pop())
 		grouped_runs.append(daily_runs)
 		day -= timedelta(days=1)
+
 	grouped_runs.reverse()
 	return grouped_runs
 
@@ -61,15 +62,76 @@ def total_daily_distances(runs, start_date=None, end_date=date.today()):
 	for each day between start_date and end_date (inclusive)
 
 	kw args:
-	runs - a list of Run objects
+		runs - a list of Run objects
 
-	start_date - a datetime object that specifies which date the function
-		should begin grouping runs together at, defaults to the first date the
-		user has a run recorded at
+		start_date - a datetime object that specifies which date the function
+			should begin grouping runs together at, defaults to the first date the
+			user has a run recorded at
 
-	end_date - a datetime object that specifies which date the function
-		should stop grouping runs together at, defaults to the current day
+		end_date - a datetime object that specifies which date the function
+			should stop grouping runs together at, defaults to the current day
 
 	'''
-	grouped_runs = group_runs(runs, start_date=start_date, end_date=end_date, distances=True)
+	grouped_runs = group_runs_daily(runs, start_date=start_date, end_date=end_date, distances=True)
+	return list(map(lambda run: sum(run), grouped_runs))
+
+def group_runs_weekly(runs, num_weeks=None, end_date=date.today(), distances=False):
+	'''
+	returns a list of lists that groups runs according to their weeks,
+	between start_date and end_date (inclusive)
+
+	kw args:
+		runs - a list of Run objects
+
+		num_weeks - an integer number of weeks to return, defaults to zero and
+			returns all weeks in record
+
+		end_date - a datetime object that specifies which date the function
+			should stop grouping runs together at, defaults to the current day
+
+		distances - a boolean.  If true, the elements of the nested lists will
+			be floats that correspond to the distances of the runs.  Otherwise,
+			the elements of the nested lists will be the Run objects themselves
+	'''
+	sorted_runs = sort_runs(runs)
+	last_monday = end_date - timedelta(days=end_date.weekday())
+
+	if num_weeks:
+		start_date = last_monday - timedelta(days=7 * (num_weeks - 1))
+	else:
+		start_date = sorted_runs[0].started_at.date()
+
+	grouped_runs = []
+	day = last_monday
+	while day >= start_date - timedelta(days=start_date.weekday()):
+		weekly_runs = []
+		while len(sorted_runs) and sorted_runs[-1].started_at.date() >= day:
+			if distances:
+				weekly_runs.append(float(sorted_runs.pop().distance))
+			else:
+				weekly_runs.append(sorted_runs.pop())
+
+		grouped_runs.append(weekly_runs)
+		day -= timedelta(days=7)
+
+	grouped_runs.reverse()
+	return grouped_runs
+
+def total_weekly_distances(runs, num_weeks=None, end_date=date.today()):
+	'''
+	returns a list of floats, where the floats represent total running distances
+	for each day between start_date and end_date (inclusive)
+
+	kw args:
+		runs - a list of Run objects
+
+		start_date - a datetime object that specifies which date the function
+			should begin grouping runs together at, defaults to the first date the
+			user has a run recorded at
+
+		end_date - a datetime object that specifies which date the function
+			should stop grouping runs together at, defaults to the current day
+
+	'''
+	grouped_runs = group_runs_weekly(runs, num_weeks=num_weeks, end_date=end_date, distances=True)
 	return list(map(lambda run: sum(run), grouped_runs))
--- runtrack_app/routes/runs/index.py
+++ runtrack_app/routes/runs/index.py
@@ -1,5 +1,5 @@
 from runtrack_app import app
-from runtrack_app.functions import sort_runs, group_runs, total_daily_distances
+from runtrack_app.functions import total_daily_distances, total_weekly_distances, sort_runs
 from flask import render_template, url_for, request
 from flask_login import current_user, login_required
 from datetime import date, datetime, timedelta
@@ -9,7 +9,11 @@ import calendar
 @app.route("/index")
 @login_required
 def index():
-	today = datetime.today().date()
+	user = current_user
+	today = date.today()
+
+	# Get daily distances
+	daily_runs = total_daily_distances(user.runs, start_date=today - timedelta(days=6))
 
 	# Get last 7 weekdays
 	days = []
@@ -17,14 +21,37 @@ def index():
 		day = day_int % 7
 		days.append(calendar.day_abbr[day])
 
-	# Get daily distances
-	runs = total_daily_distances(current_user.runs, start_date=today - timedelta(days=6))
+	# Get weekly distances (all time)
+	alltime_runs = total_weekly_distances(user.runs)
 
-	labels = ["Week 1", "Week 2", "Week 3", "Week 4"]
-	values = [23, 20, 33, 23]
+	# Get names of recorded weeks (min 4 weeks)
+	if len(alltime_runs) > 3:
+		first_date = sort_runs(user.runs)[0].started_at.date()
+		first_monday = first_date - timedelta(days=first_date.weekday())
+	else:
+		first_monday = today - timedelta(days=today.weekday() + 21)
+		alltime_runs = [[]] * (4 - len(alltime_runs)) + alltime_runs
+
+	alltime_weeks = []
+	monday = first_monday
+	while monday <= today:
+		interval = "{} {} - ".format(calendar.month_abbr[monday.month], monday.day)
+		saturday = monday + timedelta(days=6)
+		if monday.month == saturday.month:
+			interval += str(saturday.day)
+		else:
+			interval += "{} {}".format(calendar.month_abbr[saturday.month], saturday.day)
+		alltime_weeks.append(interval)
+		monday += timedelta(days=7)
+
+	# Get running and name data for last 4 weeks
+	weeks = alltime_weeks[-4:]
+	weekly_runs = alltime_runs[-4:]
 
 	return render_template("runs/index.html", 
-		values=values, 
-		labels=labels,
 		days=days,
-		runs=runs)
+		daily_runs=daily_runs,
+		weeks=weeks,
+		weekly_runs=weekly_runs,
+		alltime_weeks=alltime_weeks,
+		alltime_runs=alltime_runs)
