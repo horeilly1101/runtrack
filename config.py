c5223e88e501ed0b3f86423d608e2b45930269c1
Hugh O'Reilly <horeilly1101@gmail.com>
2018-09-10 23:42:47 -0500

    heroku db changes

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
+import psycopg2
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
-	# Allows me to work locally with a PostgreSQl db
-	SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost:5555/postgres"
+	# Allows me to work with a PostgreSQl db
+	DATABASE_URL = os.environ['DATABASE_URL']
+	conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
