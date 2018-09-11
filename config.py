78657010e43e4166674f41fb40782d9fb48ba8b9
Hugh O'Reilly <horeilly1101@gmail.com>
2018-09-11 00:15:24 -0500

    add L

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import psycopg2
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
 	# Allows me to work with a PostgreSQl db
-	DATABASE_URI = os.environ['DATABASE_URL']
+	DATABASE_URL = os.environ['DATABASE_URL']
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 	conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
